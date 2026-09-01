8589374f780f64a2e8fb77b5107d1212cbb8f0fd
Vishal Kashyap <kashyapvishal743@gmail.com>
2026-09-02 01:51:11 +0530

    Terraform correction

--- Infrastructure/managed_instance.tf
+++ Infrastructure/managed_instance.tf
@@ -1,5 +1,5 @@
 # Reference existing SQL Managed Instance
-data "azurerm_sql_managed_instance" "existing_instance" {
+data "azurerm_mssql_managed_instance" "existing_instance" {
   name                = "db-sql-mi-0569414"
   resource_group_name = "Az-DevOps-RG"
 }
@@ -7,16 +7,16 @@ data "azurerm_sql_managed_instance" "existing_instance" {
 # Output the connection string for the app
 output "managed_instance_id" {
   description = "Existing Managed Instance ID"
-  value       = data.azurerm_sql_managed_instance.existing_instance.id
+  value       = data.azurerm_mssql_managed_instance.existing_instance.id
 }
 
 output "managed_instance_fqdn" {
   description = "Fully Qualified Domain Name of Managed Instance"
-  value       = data.azurerm_sql_managed_instance.existing_instance.fqdn
+  value       = data.azurerm_mssql_managed_instance.existing_instance.fqdn
 }
 
 output "connection_string" {
   description = "Connection string for the managed instance"
-  value       = "Server=tcp:${data.azurerm_sql_managed_instance.existing_instance.fqdn},3342;Initial Catalog=<your_database>;User Id=<your_username>;Password=<your_password>;"
+  value       = "Server=tcp:${data.azurerm_mssql_managed_instance.existing_instance.fqdn},3342;Initial Catalog=<your_database>;User Id=<your_username>;Password=<your_password>;"
   sensitive   = true
 }
